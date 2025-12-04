916691f657ae59444914cda96b0bca6ed94c2d35
Gobyko23 <anawin.mayomthong23@gmail.com>
2025-12-04 08:21:17 +0700

    Add SFX for interact
    UIIIIWWWW

--- Scripts/InteractObject.gd
+++ Scripts/InteractObject.gd
@@ -1,6 +1,12 @@
 extends Node3D
 
-
+@onready var Delete_time := $"../DestoryTime"
+@onready var open_BOX := $open_BOX
+@onready var BOX := $BOX
+@onready var open_SFX := $"../Open_SFX"
+func _ready():
+	# ต่อสัญญาณให้ Timer
+	Delete_time.timeout.connect(_on_destory_time_timeout)
 
 func interact_event_in():
 	print("OBJ: Body Find")
@@ -9,6 +15,11 @@ func interact_event_out():
 	print("OBJ: Out of length 555555")
 
 func interactable():
-	queue_free()
-	print("Got it!")
+	BOX.visible = false 
+	open_BOX.visible = true
+	open_SFX.play()
+	Delete_time.start()
 	
+
+func _on_destory_time_timeout():
+	queue_free()
