18dc7700dc9640e8ae6e71223073b224e20238fe
Gobyko23 <anawin.mayomthong23@gmail.com>
2025-12-05 11:11:07 +0700

    Added open box effect
    Beep

--- Scripts/InteractObject.gd
+++ Scripts/InteractObject.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
-@onready var DesTime :Timer= $DestoryTime
-
+@onready var DesTime :Timer = $DestoryTime
+@onready var Box :MeshInstance3D = $BOX
+@onready var OpenBox :MeshInstance3D = $open_BOX
+@onready var OpenSfx :AudioStreamPlayer3D = $OpenSFX
+@onready var OpenVFX :GPUParticles3D = $OpenPaticle
 func interact_event_in():
 	print("OBJ: Body Find")
 	
@@ -9,6 +12,12 @@ func interact_event_out():
 	print("OBJ: Out of length 555555")
 
 func interactable():
-	queue_free()
-	print("Got it!")
+	DesTime.start()
+	Box.visible = false
+	OpenBox.visible = true
+	get_node("HighlightMesh").queue_free()
+	OpenSfx.play()
+	OpenVFX.emitting = true
 	
+func _on_destory_time_timeout() -> void:
+	queue_free()
