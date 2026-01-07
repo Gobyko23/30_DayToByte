af3449b32e3227e5c6c937ae15e1db9a0569b022
Gobyko23 <anawin.mayomthong23@gmail.com>
2026-01-07 14:37:08 +0700

    Dammm

--- Menu_scence/MainMenuUi.gd
+++ Menu_scence/MainMenuUi.gd
@@ -1,7 +1,5 @@
 extends Control
 
-var maxSlot :int= 3
-var currentSlot :int= 0
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
@@ -11,14 +9,3 @@ func _on_option_button_pressed() -> void:
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scence/Main_Game.tscn")
-
-
-func _on_save_button_pressed() -> void:
-	if currentSlot < maxSlot:
-		SaveAndLoad.GlobalCurrentSlot += 1
-		print("Save!")
-	else:
-		print("Slot is Max")
-
-func _on_load_button_pressed() -> void:
-	SaveAndLoad.load_game(1)
