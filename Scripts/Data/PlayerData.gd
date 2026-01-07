1f545fd88b55519e3bd9ef6bbb4eabf2238ad439
Gobyko23 <anawin.mayomthong23@gmail.com>
2026-01-07 20:34:46 +0700

    Fix aalll

--- Scripts/Data/PlayerData.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-class_name Player_Data
-
-var money := 0
-var position := Vector3.ZERO 
--- Scripts/Data/TimeManager.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-class_name TimeDayManage
-
-var day := 1
-var hour := 6
-var minute := 0
